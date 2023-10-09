a9f5a3ee709594855265739afde72eaef84e77c8
MuntasirAhmadNabil <rahatdharok@gmail.com>
2023-10-09 12:48:55 +0600

    Add project files.

---
+++ OnlineShop/Models/ProductTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineShop.Models
+{
+	public class ProductTypes
+	{
+		/// <summary>
+		/// 製品 ID
+		/// </summary>
+		public int ID { get; set; }
+
+		/// <summary>
+		/// 製品タイプ
+		/// </summary>
+		public string ProductType { get; set; }
+	}
+}
